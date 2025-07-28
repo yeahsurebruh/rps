4e97a9be6b2a736f261ef9e30951d19b23bc5f39
yeahsurebruh <91254480+yeahsurebruh@users.noreply.github.com>
2025-07-29 01:10:11 +0530

    write script that gives random output r/p/s

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!\n");
+const choices = ["Rock", "Paper", "Scissors"];
+const num_choices = 3;
+
+function getComputerChoice (arr, len){
+    return arr[Math.floor(Math.random() * len)];
+}
+
+console.log(getComputerChoice(choices, num_choices));
